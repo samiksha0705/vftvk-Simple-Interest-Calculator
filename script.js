0be180c26d0e305eabb84cf60fa1599fadd4d358
samiksha0705 <86676272+samiksha0705@users.noreply.github.com>
2021-06-29 21:43:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+function cal(){
+			a=document.getElementById("amount").value;
+			b=document.getElementById("get").value;
+			c=document.getElementById("year").value;
+			result = document.getElementById("result");
+			aa=document.getElementById("aa");
+			bb=document.getElementById("bb");
+			cc=document.getElementById("cc");
+			var x=2021;
+     
+			aa.innerHTML = "If you deposit "+a;
+			bb.innerHTML = "at an interest rate of "+b+"%";
+			result.innerHTML = "You will recieve an amount of " + (a*b*c/100);
+			cc.innerHTML = "in the year "+(parseInt(x)+parseInt(c));
+		}
